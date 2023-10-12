218923307a0772d606d141866ce14566416deb14
David Borsa <david.borsa03@e-uvt.ro>
2023-10-12 21:40:10 +0300

    routes modif

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :user_information
+  resources :user_information, only: [:new,:update,:create,:destroy]
 
   get '/landing', to: 'home#landing'
   root to: "home#landing"
