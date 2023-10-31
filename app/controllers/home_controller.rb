12bc8c2826d23e9552ab29545e6911aadc8dd0e2
David Borsa <david.borsa03@e-uvt.ro>
2023-10-31 22:47:05 +0200

    added page highlight on bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
       user_skills = return_user_skills
   
       @jobs = if params[:title].present?
-        Job.where("title LIKE ?", "%#{params[:title]}%")
+        Job.where("lower(title) LIKE ?", "%#{params[:title].downcase}%")
       else
         Job.all
       end.includes(:skills).sort_by do |job|
@@ -14,12 +14,13 @@ class HomeController < ApplicationController
       end.reverse
     else
       @jobs = if params[:title].present?
-        Job.where("title LIKE ?", "%#{params[:title]}%").order(created_at: :desc)
+        Job.where("lower(title) LIKE ?", "%#{params[:title].downcase}%").order(created_at: :desc)
       else
         Job.all.order(created_at: :desc)
       end
     end
   end
+  
 
   def view_job
     @job = Job.find(params[:id])
