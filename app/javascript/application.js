5ef44bcdc8862566cbfd51c4ff6e253ecacb4da9
David Borsa <david.borsa03@e-uvt.ro>
2023-11-25 00:41:23 +0200

    another render asset correction

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
@@ -8,4 +7,3 @@
 //= require chosen-jquery
 //= require components
 //= require chosen-select
-
