1f935394c1028342f861b92741d2259ec41a2414
David Borsa <david.borsa03@e-uvt.ro>
2024-12-08 22:23:53 +0200

    fixed assets precompile

--- app/javascript/application.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require react
-//= require react_ujs
-//= require_tree .
-//= require chosen-jquery
-//= require components
-//= require chosen-select
